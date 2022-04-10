1364640465d65e07233cb795c01a0865d3c4bcbf
nikisha-scipt <nikisha_script@mail.ru>
2022-04-10 15:51:37 +0300

    0. Что такое обобщенные типы (generics) [#4952 #286979] (reload)

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -22,10 +22,12 @@ public class Generics {
 
         gen.printBoundedWildCard(first);
         gen.printBoundedWildCard(second);
+        /* gen.printBoundedWildCard(third); */
         System.out.println();
 
+        /* gen.printLowerBoundedWildCard(first); */
         gen.printLowerBoundedWildCard(second);
-
+        gen.printLowerBoundedWildCard(third);
     }
 
     public void printObject(List<?> list) {
--- src/main/java/ru/job4j/generics/Tiger.java
+++ src/main/java/ru/job4j/generics/Tiger.java
@@ -1,6 +1,6 @@
 package ru.job4j.generics;
 
-public class Tiger extends Animal {
+public class Tiger extends Predator {
 
     public Tiger(String name, Integer age, String color) {
         super(name, age, color);
