a6953d4bd6102c8bae471df82140893d2dd7ed3c
nikisha-scipt <nikisha_script@mail.ru>
2022-04-14 12:49:44 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

---
+++ src/test/java/ru/job4j/list/SimpleStackTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+
+public class SimpleStackTest {
+
+    private MyStack<Integer> myStack;
+
+    @Before
+    public void createMyStack() {
+        myStack = new SimpleStack<>();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+    }
+
+    @Test
+    public void whenPushThenPoll() {
+        SimpleStack<Integer> stack = new SimpleStack<>();
+        stack.push(1);
+        assertThat(stack.pop(), is(1));
+    }
+
+    @Test
+    public void whenPushPollThenPushPoll() {
+        SimpleStack<Integer> stack = new SimpleStack<>();
+        stack.push(1);
+        stack.pop();
+        stack.push(2);
+        assertThat(stack.pop(), is(2));
+    }
+
+    @Test
+    public void whenPushPushThenPollPoll() {
+        SimpleStack<Integer> stack = new SimpleStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.pop();
+        assertThat(stack.pop(), is(1));
+    }
+
+}
