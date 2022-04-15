0fdfe1784d5a5bd8709781bc186ef3f039ec2cf8
nikisha-scipt <nikisha_script@mail.ru>
2022-04-15 10:23:16 +0300

    7. ListIterator [#350217 #288896] (reload)

--- src/main/java/ru/job4j/list/listiterator/ListUtils.java
+++ src/main/java/ru/job4j/list/listiterator/ListUtils.java
@@ -8,21 +8,13 @@ public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator(index);
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        iterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator(index);
-        while (iterator.hasNext()) {
-            iterator.next();
-        }
+        iterator.next();
         iterator.add(value);
     }
 
