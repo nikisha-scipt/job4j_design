59ae2239f620777004a62e7f9ff1e05acdec5e21
nikisha-scipt <nikisha_script@mail.ru>
2022-05-03 12:33:16 +0300

    0.1. FileOutputStream. [#252488 #295875] (reload)

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,6 +1,9 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
 
@@ -16,9 +19,27 @@ public class ResultFile {
                 }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        /**
+         * Буфферизированная запись в файл
+         */
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("result.txt")))) {
+            int[][] arr = multiple(5);
+            out.println("It is multiple:");
+            for (int[] ints : arr) {
+                out.println("\t\t");
+                for (int anInt : ints) {
+                    out.print((anInt + " "));
+                }
+                out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static int[][] multiple(int size) {
