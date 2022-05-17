6376c939fb349bb12a23df141969f91f68747c0d
nikisha-scipt <nikisha_script@mail.ru>
2022-05-17 12:58:45 +0300

    4.2. Поиск дубликатов [#315066 #297395] (reload)

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -14,19 +14,15 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (!maps.containsKey(fileProperty)) {
-            maps.put(fileProperty, new ArrayList<>());
-            maps.get(fileProperty).add(file.toAbsolutePath());
-        } else {
-            maps.get(fileProperty).add(file.toAbsolutePath());
-        }
+        maps.putIfAbsent(fileProperty, new ArrayList<>());
+        maps.get(fileProperty).add(file.toAbsolutePath());
         return super.visitFile(file, attrs);
     }
 
     public void printFiles() {
-        for (Map.Entry<FileProperty, List<Path>> entry : maps.entrySet()) {
-            if (entry.getKey().getSize() < 2) {
-                System.out.println(entry.getValue());
+        for (List<Path> entry : maps.values()) {
+            if (entry.size() >= 2) {
+                System.out.println(entry);
             }
         }
     }
