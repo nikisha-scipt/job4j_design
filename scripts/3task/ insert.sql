02216491a536e47232bd83e5aa86c02985e5f99f
nikisha-scipt <nikisha_script@mail.ru>
2022-05-22 10:41:59 +0300

    1. Схема прав пользователи и роли. [#1093 #303560] (reload)

--- scripts/3task/ insert.sql
+++ scripts/3task/ insert.sql
@@ -1,14 +1,14 @@
-insert into users (name, surname)
-values ('Danil', 'Nikishin');
+insert into users (name, surname, id_role)
+values ('Danil', 'Nikishin', 1);
 
-insert into role (name, id_user)
-values ('programmer', 1);
+insert into role (name)
+values ('programmer');
 
 insert into roles (name, id_role, id_user)
 values ('high', 1, 1);
 
-insert into item (name, id_user)
-values ('one item', 1);
+insert into item (name, id_user, id_category, id_state)
+values ('one item', 1, 1, 1);
 
 insert into comments (name, id_item)
 values ('first comment', 1);
@@ -16,8 +16,8 @@ values ('first comment', 1);
 insert into attachs (name, id_item)
 values ('first attachs', 1);
 
-insert into category (name, id_item)
-values ('first attachs', 1);
+insert into category (name)
+values ('first attachs');
 
-insert into state (name, id_item)
-values ('first attachs', 1);
+insert into state (name)
+values ('first attachs');
--- scripts/3task/create.sql
+++ scripts/3task/create.sql
@@ -1,26 +1,40 @@
+users - role = many-to-one (many - со стороны внешнего ключа)
+role - rules = many-to-many
+item - users = many-to-one
+item - comments = one-to-many
+item - attachs = one-to-many
+item - category = many-to-one
+item - state = many-to-one
+
 create table users (
     id_user serial primary key,
     name varchar(50) not null
     surname varchar(50) not null
+    id_role int references role(id_role)
 );
 
 create table role (
     id_role serial primary key,
     name text not null
-    id_user int references users(id_user)
 );
 
 create table roles (
-    roles serial primary key,
-    name text not null,
-    id_role int references role(id_role),
-    id_user int references users(id_user)
+    id_roles serial primary key,
+    name text not null
 );
+
+create table role_roles (
+    id_role_roles serial primary key,
+    id_role int references role(id_role),
+    id_roles int references roles(id_roles)
+)
 
 create table item (
     id_item serial primary key,
     name text not null,
-    id_user int references users(id_user)
+    id_user int references users(id_user),
+    id_item int references category(id_category),
+    id_state int references state(id_state)
 );
 
 create table comments (
@@ -37,14 +51,12 @@ create table attachs (
 
 create table category (
     id_category serial primary key,
-    name text not null,
-    id_item int references item(id_item)
+    name text not null
 );
 
 create table state (
     id_state serial primary key,
-    name text not null,
-    id_item int references item(id_item)
+    name text not null
 );
 
 
