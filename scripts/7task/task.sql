75d46e60300a0faec5294a0a8b39f14d870097d1
nikisha-scipt <nikisha_script@mail.ru>
2022-05-23 11:09:34 +0300

    3. Фильтры [#1732 #303844] (reload)

--- scripts/7task/task.sql
+++ scripts/7task/task.sql
@@ -1,23 +1,16 @@
-CREATE TABLE IF NOT EXISTS public.product
-(
-    id integer NOT NULL DEFAULT nextval('product_id_seq'::regclass),
-    name text COLLATE pg_catalog."default" NOT NULL,
-    type_id integer,
-    expired_date date,
-    price double precision,
-    CONSTRAINT product_pkey PRIMARY KEY (id),
-    CONSTRAINT product_type_id_fkey FOREIGN KEY (type_id)
-        REFERENCES public.type (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
+create table type (
+    id serial primary key,
+    name varchar(30)
+);
 
-CREATE TABLE IF NOT EXISTS public.type
-(
-    id integer NOT NULL DEFAULT nextval('type_id_seq'::regclass),
-    name text COLLATE pg_catalog."default",
-    CONSTRAINT type_pkey PRIMARY KEY (id)
-)
+create table product (
+    id serial primary key,
+    name varchar(30),
+    type_id int references type(id),
+    expired_date date,
+    price float
+);
+
 
 insert into type (name) values ('СЫР'), ('МОЛОКО');
 insert into product (name, type_id, expired_date, price)
@@ -25,7 +18,6 @@ values ('Сыр плавленный', 1, date '2020-01-01', 350),
 ('Молоко', 2, date '2021-01-01', 450),
 ('Сыр моцарелла', 1, date '2020-02-01', 550),
 ('Мороженное', 2, date '2021-07-01', 80);
-
 
 
 1)
@@ -44,10 +36,10 @@ where type.name like '%мороженое%';
 select * from product
 join type
 on product.type_id = type.id
-where product.expired_date > '2019-12-01';
+where product.expired_date > current_date;
 
 4)
-select max(price) from product;
+select max(price) from product limit 1;
 
 5)
 select t.name as nameOftype, count(p.name)
