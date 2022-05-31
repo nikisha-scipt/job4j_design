3a15a8264013129cc5cfe5093438f7688bdb2e87
nikisha-scipt <nikisha_script@mail.ru>
2022-05-31 10:38:20 +0300

    2. SQL Select by company [#2760 #307173] (reload)

--- scripts/10task/task.sql
+++ scripts/10task/task.sql
@@ -64,7 +64,7 @@ select c.name, p.name from person as p
 inner join company as c
 on c.id = p.company_id
 group by p.name, c.name, p.company_id
-having p.company_id!= 1;
+having p.company_id!= 5;
 
 2)
 select c.name, count(p.company_id) from person as p
