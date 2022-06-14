3ecd2f483651983af411ffe9c89c18d41786fa89
nikisha-scipt <nikisha_script@mail.ru>
2022-06-14 17:30:40 +0300

    1. Отчеты. [#850] (reload)

--- src/main/java/ru/job4j/srp/ReportHr.java
+++ src/main/java/ru/job4j/srp/ReportHr.java
@@ -1,6 +1,5 @@
 package ru.job4j.srp;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
--- src/test/java/ru/job4j/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/srp/ReportEngineTest.java
@@ -14,12 +14,12 @@ public class ReportEngineTest {
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report engine = new ReportEngine(store);
-        String expect = "Name; Hired; Fired; Salary;" +
-                worker.getName() + ";" +
-                worker.getHired() + ";" +
-                worker.getFired() + ";" +
-                worker.getSalary() + ";" +
-                System.lineSeparator();
+        String expect = "Name; Hired; Fired; Salary;"
+                + worker.getName() + ";"
+                + worker.getHired() + ";"
+                + worker.getFired() + ";"
+                + worker.getSalary() + ";"
+                + System.lineSeparator();
         assertThat(engine.generate(em -> true), is(expect));
     }
 
@@ -30,17 +30,17 @@ public class ReportEngineTest {
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report html = new ReportHtml(store);
-        String expect = "<!DOCTYPE html>" + System.lineSeparator() +
-                "<html lang =\"ru\">" + System.lineSeparator() +
-                "<body>" + System.lineSeparator() +
-                "Name; Hired; Fired; Salary" + System.lineSeparator() +
-                worker.getName() + ";" +
-                worker.getHired() + ";" +
-                worker.getFired() + ";" +
-                worker.getSalary() + ";" +
-                System.lineSeparator() +
-                "</body>" + System.lineSeparator() +
-                "</html>" + System.lineSeparator();
+        String expect = "<!DOCTYPE html>" + System.lineSeparator()
+               + "<html lang =\"ru\">" + System.lineSeparator()
+               + "<body>" + System.lineSeparator()
+               + "Name; Hired; Fired; Salary" + System.lineSeparator()
+               + worker.getName() + ";"
+               + worker.getHired() + ";"
+               + worker.getFired() + ";"
+               + worker.getSalary() + ";"
+               + System.lineSeparator()
+                + "</body>" + System.lineSeparator()
+               + "</html>" + System.lineSeparator();
         assertThat(html.generate(em -> true), is(expect));
     }
 
@@ -51,12 +51,12 @@ public class ReportEngineTest {
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report salary = new ReportSalary(store, 45);
-        String expect = "Name; Hired; Fired; Salary;" +
-                worker.getName() + ";" +
-                worker.getHired() + ";" +
-                worker.getFired() + ";" +
-                worker.getSalary() * 45 + ";" +
-                System.lineSeparator();
+        String expect = "Name; Hired; Fired; Salary;"
+                + worker.getName() + ";"
+                + worker.getHired() + ";"
+                + worker.getFired() + ";"
+                + worker.getSalary() * 45 + ";"
+                + System.lineSeparator();
         assertThat(salary.generate(em -> true), is(expect));
     }
 
@@ -69,13 +69,13 @@ public class ReportEngineTest {
         store.add(worker1);
         store.add(worker2);
         Report hr = new ReportHr(store);
-        String expect = "Name;Salary;" + System.lineSeparator() +
-                worker2.getName() + ";" +
-                worker2.getSalary() + ";" +
-                System.lineSeparator() +
-                worker1.getName() + ";" +
-                worker1.getSalary() + ";" +
-                System.lineSeparator();
+        String expect = "Name;Salary;" + System.lineSeparator()
+                + worker2.getName() + ";"
+                + worker2.getSalary() + ";"
+                + System.lineSeparator()
+                + worker1.getName() + ";"
+                + worker1.getSalary() + ";"
+                + System.lineSeparator();
         assertThat(hr.generate(em -> true), is(expect));
     }
 
