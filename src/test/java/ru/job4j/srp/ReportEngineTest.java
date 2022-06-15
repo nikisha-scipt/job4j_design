87380b17fd24610894eebc3abef473e73bdb5de3
nikisha-scipt <nikisha_script@mail.ru>
2022-06-15 10:45:52 +0300

    1. Отчеты. [#850 #312390] (reload)

--- src/test/java/ru/job4j/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/srp/ReportEngineTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.srp;
 
-import org.junit.Ignore;
 import ru.job4j.solid.srp.*;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -34,31 +33,34 @@ public class ReportEngineTest {
     }
 
     @Test
-    @Ignore
     public void whenReportHtml() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report html = new ReportHtml(store);
-        System.out.println(html.generate(emp -> true));
         StringBuilder expect = new StringBuilder()
-                .append("<!DOCTYPE html>").append(System.lineSeparator())
-                .append("<html lang =ru>").append(System.lineSeparator())
-                .append("<body>").append(System.lineSeparator())
-                .append("Name;Hired;Fired;Salary").append(System.lineSeparator())
+                .append("<!DOCTYPE html>")
+                .append(System.lineSeparator())
+                .append("<html lang =ru>")
+                .append(System.lineSeparator())
+                .append("<body>")
+                .append(System.lineSeparator())
+                .append("Name;Hired;Fired;Salary")
+                .append(System.lineSeparator())
                 .append(worker.getName()).append(";")
                 .append(worker.getHired()).append(";")
                 .append(worker.getFired()).append(";")
                 .append(worker.getSalary()).append(";")
                 .append(System.lineSeparator())
-                .append("</body>").append(System.lineSeparator())
-                .append("</html>").append(System.lineSeparator());
-        assertThat(html.generate(em -> true), is(expect));
+                .append("</body>")
+                .append(System.lineSeparator())
+                .append("</html>")
+                .append(System.lineSeparator());
+        assertThat(html.generate(em -> true), is(expect.toString()));
     }
 
     @Test
-    @Ignore
     public void whenReportSalary() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
@@ -73,11 +75,10 @@ public class ReportEngineTest {
                 .append(worker.getFired()).append(";")
                 .append(worker.getSalary() * 45).append(";")
                 .append(System.lineSeparator());
-        assertThat(salary.generate(em -> true), is(expect));
+        assertThat(salary.generate(em -> true), is(expect.toString()));
     }
 
     @Test
-    @Ignore
     public void whenReportHr() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
@@ -87,14 +88,14 @@ public class ReportEngineTest {
         store.add(worker2);
         Report hr = new ReportHr(store);
         StringBuilder expect = new StringBuilder();
-        expect.append("Name;Salary;").append("\n");
+        expect.append("Name;Salary;").append(System.lineSeparator());
         expect.append(worker2.getName()).append(";");
         expect.append(worker2.getSalary()).append(";");
-        expect.append("\n");
+        expect.append(System.lineSeparator());
         expect.append(worker1.getName()).append(";");
         expect.append(worker1.getSalary()).append(";");
-        expect.append("\n");
-        assertThat(hr.generate(em -> true), is(expect));
+        expect.append(System.lineSeparator());
+        assertThat(hr.generate(em -> true), is(expect.toString()));
     }
 
     @Test
