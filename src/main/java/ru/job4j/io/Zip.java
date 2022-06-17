96b409e897da11a30fd6e54bdc10634ddbb55990
nikisha-scipt <nikisha_script@mail.ru>
2022-06-17 11:17:06 +0300

    5.2. Архивировать проект [#861 #300231] (reload)

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -2,56 +2,91 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<Path> sources, Path target) {
-        packSingleFile(sources, target);
-    }
-
-    public void packSingleFile(List<Path> sources, Path target) {
-        for (Path file : sources) {
-            try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(String.valueOf(target))))) {
-                zip.putNextEntry(new ZipEntry(file.toFile().getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(file)))) {
+    public void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                     zip.write(out.readAllBytes());
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static void valid(ArgsName args) throws IllegalAccessException {
-        if (!args.get("d").equals("directory")) {
-            throw new IllegalAccessException("Invalid directory");
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        if (!args.get("e").equals("exclude")) {
-            throw new IllegalAccessException("Invalid exclude");
+    }
+
+
+    private static class Valid {
+
+        private final ArgsName args;
+        private final int size;
+
+        public Valid(String[] args) {
+            this.args = ArgsName.of(args);
+            this.size = args.length;
         }
-        if (!args.get("o").equals("output")) {
-            throw new IllegalAccessException("Invalid output");
+
+        private boolean checkSize() {
+            return size == 3;
         }
+
+        public String dir() {
+            return args.get("d");
+        }
+
+        public String exclude() {
+            return args.get("e");
+        }
+
+        public String out() {
+            return args.get("o");
+        }
+
+        public boolean isValid() {
+            try {
+                checkSize();
+                dir();
+                exclude();
+                out();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
     }
 
 
     public static void main(String[] args) throws IOException, IllegalAccessException {
-        if (args.length != 3) {
-            throw new IllegalArgumentException("format: -d=c:\\project -e=.class -o=project.zip");
+        Valid valid = new Valid(args);
+        if (valid.isValid()) {
+            Path root = Paths.get(valid.dir());
+            List<Path> sourcesPath = Search.search(root, p ->
+                    !p.toFile().getName().endsWith(valid.exclude()));
+            List<File> sources = sourcesPath.stream()
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
+            new Zip().packFiles(sources, new File(valid.out()));
         }
-        ArgsName argsName = ArgsName.of(args);
-        valid(argsName);
-        List<Path> files = Search.search(Path.of(argsName.get("d")), element -> !element.toFile().getName().endsWith(argsName.get("e")));
-        new Zip().packFiles(files, Path.of(argsName.get("o")));
-
-        /* Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        ); */
     }
 
 }
